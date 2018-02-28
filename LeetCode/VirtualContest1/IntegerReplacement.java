3722e3dd78751601f28d72527518a6569267c6f0
shivani26shinde <shindeshivani26@gmail.com>
2018-02-27 23:34:55 -0600

    LeetCode Virtual Contest 1: Integer Replacement

---
+++ LeetCode/VirtualContest1/IntegerReplacement.java
@@ -0,0 +1,16 @@
+LeetCode --> Virtual Contest
+
+class IntegerReplacement {
+    public int integerReplacement(int n) {
+        if(n==1){
+            return 0;
+        }
+        if(n%2 == 0){
+            return 1 + integerReplacement(n/2);
+        }
+        if(n%2 == 1){
+            return 1 + Math.min(integerReplacement(n+1), integerReplacement(n-1));
+        }
+        return 0;
+    }
+}
