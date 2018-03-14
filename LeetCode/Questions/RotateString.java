13329ace7aba5e6e947e7b90ec131f7a0c2ec2f2
shivani26shinde <shindeshivani26@gmail.com>
2018-03-13 23:45:16 -0500

    LeetCode Q.796 completed with N^2 complexity

--- LeetCode/Questions/RotateString.java
+++ LeetCode/Questions/RotateString.java
@@ -1,5 +1,6 @@
 // LeetCode --> Q.796 --> Rotate String
 
+// In progress
 class RotateString {
     public boolean rotateString(String A, String B) {
         if(A.length() != B.length()){
@@ -13,6 +14,24 @@ class RotateString {
             }
             return true;
         }
+        return false;
+    }
+}
+
+// Correct Code
+class RotateString {
+    public boolean rotateString(String A, String B) {
+        if(A.length() != B.length()){
+            return false;
+        }
+        for(int i=0;i<A.length();i++){
+			String sub1 = A.substring(0,i);
+			String sub2 = A.substring(i,A.length());
+			String str = sub2+sub1;
+            if(B.matches(str)){
+                return true;
+            }
+		}
         return false;
     }
 }
