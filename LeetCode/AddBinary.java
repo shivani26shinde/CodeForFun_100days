944611bce5860d6515409494e9d7ad816ab79eaf
shivani26shinde <shindeshivani26@gmail.com>
2018-04-11 21:11:56 -0500

    Add Binary completed

--- LeetCode/AddBinary.java
+++ LeetCode/AddBinary.java
@@ -3,14 +3,33 @@
 class AddBinary {
     public String addBinary(String a, String b) {
         int num = 0;
-        int start1 = 0;
-        int start2 = 0;
+        int start1 = a.length()-1;
+        int start2 = b.length()-1;
+        int val1 = 0;
+        int val2 = 0;
         StringBuilder sb = new StringBuilder();
-        while(start1 < a.length() || start2 < b.length()){
-            int val1 = Character.getNumericValue(a.charAt(start1)); 
-            int val2 = Character.getNumericValue(b.charAt(start2));
-            int sum = num+val1+val2;
+        while(start1 >= 0 || start2 >= 0){
+            if(start1 >= 0){
+                val1 = Character.getNumericValue(a.charAt(start1));
+                start1--;
+            }
+            else{
+                val1 = 0;
+            }
+            if(start2 >= 0){
+                val2 = Character.getNumericValue(b.charAt(start2));
+                start2--;
+            }
+            else{
+                val2 = 0;
+            }
+            int sum = (num+val1+val2)%2;
+            sb.append(sum);
+            num = (num+val1+val2)/2;
         }
-        return sb.toString();
+        if(num > 0){
+            sb.append(num);
+        }
+        return sb.reverse().toString();
     }
 }
