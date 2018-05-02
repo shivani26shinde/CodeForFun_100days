12f3ce410ba1b737e95543e7d7bd9b8247690adc
shivani26shinde <shindeshivani26@gmail.com>
2018-05-01 23:17:30 -0500

    algo completed

---
+++ Algorithms/TallPeople.java
@@ -0,0 +1,41 @@
+# Tall People
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+public class TallPeople{
+	public int[] getPeople(String[] people){
+        ArrayList<ArrayList<Integer>> list = new ArrayList<>();
+    	for(int i=0;i<people.length;i++){
+        	ArrayList<Integer> temp = new ArrayList<>();
+            String s = people[i];
+            for(int j=0;j<s.length();j++){
+                StringBuilder sb = new StringBuilder();
+            	while(j<s.length() && s.charAt(j) != ' '){
+                    sb.append(s.charAt(j));
+                    j++;
+                }
+                temp.add(Integer.parseInt(sb.toString()));
+            }
+            list.add(temp);
+        }
+        ArrayList<Integer> row = new ArrayList<>();
+        for(int i=0;i<list.size();i++){
+        	ArrayList<Integer> temp1 = list.get(i);
+            row.add(Collections.min(temp1));
+        }
+        ArrayList<Integer> col = new ArrayList<>();
+        for(int i=0;i<list.get(0).size();i++){
+            ArrayList<Integer> temp3 = new ArrayList<>();
+        	for(int j=0;j<list.size();j++){
+            	temp3.add(list.get(j).get(i));
+            }
+            col.add(Collections.max(temp3));
+        }
+        int[] finalAns = new int[2];
+        finalAns[0] = Collections.max(row);
+        finalAns[1] = Collections.min(col);
+        return finalAns;
+    }
+}
